a1201c7bd781a041ce133d85f7a74cda829ebb52
jayakrishnandamodaran <jayakrishnan.d@uroleaf.com>
2014-10-23 16:57:10 +0530

    started scrapy project.

---
+++ wynwoodmapextractor/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for wynwoodmapextractor project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'wynwoodmapextractor'
+
+SPIDER_MODULES = ['wynwoodmapextractor.spiders']
+NEWSPIDER_MODULE = 'wynwoodmapextractor.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'wynwoodmapextractor (+http://www.yourdomain.com)'
